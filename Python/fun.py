79e92cacd736c88064869918138ae894df2e546e
hydtrng <hydtrng@SONATA.LOCAL>
2022-08-25 20:06:15 +0530

    new one

---
+++ Python/fun.py
@@ -0,0 +1,33 @@
+#Array
+from array import *
+chandu = array('i', [780,6,401,56,53,37628,87])
+print(chandu[2])
+print(chandu[1]+chandu[2])
+print(chandu[1]%2)
+print(chandu[:-1])
+print(type(chandu))
+
+#list
+chandu=[21,'chandukasani',2127,'kasani',"sonata employee"]
+print(chandu[2])
+print(chandu[0]+chandu[2])
+print(chandu[2]%2)
+print(chandu[:-1])
+print(type(chandu))
+
+#tuple
+s=(2127,'chandu',376,'pavan')
+print(s[3])
+print(s[0]+s[1])
+print(s[2]%2)
+print(s[:-1])
+
+
+#set
+a={6,'chandu',46762,'kasani',12420,"sonata employee"}
+print(a)
+print(type(a))
+
+#dictionary
+c={"chandu": "21/12/2000","pavan": "25/10/2000",'swthi': "08/08/2000",'sandhya':'13-09-2000'}
+print(i["c"])
